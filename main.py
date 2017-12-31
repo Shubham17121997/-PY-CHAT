5c7a77a4ba60d9b273dad8f24362791c058898bd
shubham11602152 <shubhammehta.333@gmail.com>
2017-12-31 19:19:21 +0530

    My first SPY_CHAT file

---
+++ main.py
@@ -0,0 +1,9 @@
+print ("Hello!")
+print ('yo! what\'s up')
+print ('Let\'s get started')
+print ('Welcome to spy chat , firstly I need to know you ha..ha..ha.... ')
+spy_name = input ("what is your name ?\n")
+print ('Welcome '+spy_name+'.\nGlad to have you back with us.')
+spy_salutation = input ('what you would like to be called ( Mr. or Ms.)?\n')
+spy_name = spy_salutation+'.'+spy_name
+print ('Alright ' +spy_name+' I\'d like to know a little bit more about you')
