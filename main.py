88f6cb27495ba49ce66547310840d765b5b0a411
shubham11602152 <shubhammehta.333@gmail.com>
2018-01-10 00:44:48 +0530

    SPY_CHAT - second module completed

--- main.py
+++ main.py
@@ -1,20 +1,95 @@
 from spy_details import spy_name , spy_salutation , spy_age , spy_rating
 
+# [ Defining - List ]
+STATUS_MESSAGES = ["Born leader" , "Thug life" , "James bond" ]
+
+friends_name = []
+friends_age = []
+friends_rating = []
+friends_is_online = []
+
 print ("Hello!!!!\nyo! what\'s up")
 print ('Let\'s get started')
-print ('Welcome to spy chat , firstly I need to know you ha..ha..ha.... ')
+print ('Welcome to spy chat , firstly I need to know you... ')
 
 # This is a question for spy to to continue as default user (Y) or define a new user.
 question = "Do you want to continue as %s?(Y/N)\n" % (spy_name) # spy_name was updated with salutation in [ spy_details.py ]
 # question = "Continue as " + spy_salutation + " " + spy_name + "(Y/N)?"
-
 existing_spy = raw_input(question)
-# [ Defining - Function ] so that it can be called anywhere.
+
+
+# [ Defining - Function ] add_status() so that it can be called anywhere.
+def add_status(current_status):
+    updated_status = None
+
+    if current_status != None:
+        print 'Your current status message is %s \n' % (current_status)
+    else:
+        print 'You don\'t have any status message currently \n'
+
+    default = raw_input("do you want to select from older status (Y/N) : ")
+
+    if (default.upper() == 'N'):
+        new_status = raw_input("what status message do you want to set ? : ")
+
+        if len(new_status) > 0 :
+            updated_status = new_status
+            # We could also use[ .append() ] in place of [.insert()] ,but it inserts item at the end of the list
+            # With [ insert(index,item) ] we can provide the item with index where it should be stored
+            STATUS_MESSAGES.insert(0,updated_status)
+            # status.append(update_status)
+            print (STATUS_MESSAGES)
+
+    elif (default.upper() == "Y") :
+        item_position = 1          #[ Initializing ] the status message index to 1 .
+
+        for message in STATUS_MESSAGES :
+            print '%d. %s' % (item_position, message)
+            # print ( str(item_position) + "." + (message) )
+            item_position += 1         #[ Incrementing ] status index
+            # item_position = item_position + 1
+
+        message_selection = input("\nChoose from the above messages : ")
+        if len(STATUS_MESSAGES) >= message_selection :
+            updated_status = STATUS_MESSAGES[message_selection - 1]
+
+    return updated_status
+# [END] of function block add_status() .
+
+
+# [Defining - Function] add_friend() .
+def add_friend():
+
+    new_name = raw_input("Please add your friend's name : ")
+    new_salutation = raw_input("Are they Mr. or Ms.? : ")
+
+    # new_name = new_salutation + "." + new_name
+    new_name = "%s.%s"%(new_salutation , new_name)
+
+    new_age = input("Age         :")
+
+    new_rating = input("Spy rating : ")
+
+
+    if len(new_name) > 0 and new_age > 12 and new_rating >= spy_rating:
+        friends_name.append(new_name)
+        friends_age.append(new_age)
+        friends_rating.append(new_rating)
+        friends_is_online.append(True)
+        print 'Friend Added!'
+    else:
+        print 'Sorry! Invalid entry. We can\'t add spy with the details you provided'
+
+    return len(friends_name)
+# [END] of function block add_friend() .
+
+
+# [Defining - Function] start_chat() .
 def start_chat(spy_name , spy_age , spy_rating):
-    status = None
+    current_status = None
     # No need to update spy_name as it was updated in [ spy_details.py ] file.
     if (12 < spy_age < 50) :
-        print("Authentication completed.\n Welcome %s\n  Age    : %d\n  Rating : %.1f\n Proud to have you on-board."
+        print("Authentication completed.\n Welcome %s\n  Age    : %d\n  Rating : %.1f\n Proud to have you back."
               % (spy_name,spy_age,spy_rating))
 
         # { Initialized } show to True so that We can keep showing the menu again after
@@ -22,15 +97,20 @@ def start_chat(spy_name , spy_age , spy_rating):
         show = True
         #[ While ] statement [executes] till (show) is {True} and [terminates] when (show) is {False}
         while show :
-            menu_choices = input ("what do you want to do ?\n 1. Add a status update\n 2. Exit\n")
+            menu_choices = input ("what do you want to do ?\n 1. Add a status update\n 2. Add friend\n 3. Send a secret message \n 4. Read a secret message \n 5. Read Chats from a user \n 6. [Exit]\n")
             # [Add/UPDATE  status]
             if (menu_choices == 1) :
-                status = raw_input ("Write your status : ")
-                print ("Status updated to...[%s]" % (status))
-            # [EXIT]
+                current_status = add_status(current_status) #status = raw_input ("Write your status : ")
+                print ("...............................Status updated to...[%s]" % (current_status))
+            # [Add friend]
             elif (menu_choices == 2) :
-                print "Get the hell..out of here ....you..idiot."  # "Thanks for visiting spy_chat! Logging out..."
+                number_of_friends = add_friend()
+                print 'You have %d friends' % (number_of_friends)
+            # [EXIT]
+            elif (menu_choices == 6) :
+                print "Thanks for visiting spy_chat!    Logging out..."
                 show = False
+            # [END] of function block start_chat().
 
 if (existing_spy.upper() == "Y") :
     #Continue with the default user/details imported from the helper file.
@@ -85,14 +165,14 @@ else :
            spy_is_online = True
 
            # Using {PLACE HOLDERS}
-           print("Authentication completed.\n Welcome %s\n  Age    : %d\n  Rating : %.1f\n Proud to have you back." % (spy_name,spy_age,spy_rating))
+           print("Authentication completed.\n Welcome %s\n  Age    : %d\n  Rating : %.1f\n Proud to have you on-board." % (spy_name,spy_age,spy_rating))
            # We can also use give below statement, but according to zen of python we use above one ...
            # String integer objects typecasting using str() , repr() or backtick.
            #print("Authentication completed.\n Welcome " + spy_name + "\n Age : " + str(spy_age) + "\n Rating : " + str(spy_rating) + "\n Spy is online : " + str(spy_is_online))
            #print("Authentication completed.\n Welcome " + spy_name + "\n Age : " + repr(spy_age) + "\n Rating : " + repr(spy_rating))
            #print("Authentication completed.\n Welcome " + spy_name + "\n Age : " + `spy_age` + "\n Rating : " + `spy_rating` + ")
        else :
-         print ("SORRY..! you are not eligible yet.\nplay (chidi..udd..kaa..udd.) ha.ha.haaa..")
+         print ("SORRY..! you are not eligible yet. ")  #"play (chidi..udd..kaa..udd.) ha.ha.haaa..")
 
     else :
         print ('SORRY..! NO NAME !. \nPLEASE!! TRY AGAIN..')
--- spy_details.py
+++ spy_details.py
@@ -2,5 +2,6 @@ spy_name = 'Shubham'
 spy_salutation = 'Mr.'
 spy_age = 20
 spy_rating = 5
+spy_is_online = True
 # [UPDATING] name to decrease coding in main file **
 spy_name=spy_salutation + spy_name
